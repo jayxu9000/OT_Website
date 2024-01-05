92ac354b1da21abe3126beb28a8d461b486c3173
yuanjiex21 <jayxu9000@gmail.com>
2024-01-05 02:31:59 -0500

    Debugged verification page

--- OT_Website_Frontend/src/pages/Verification.jsx
+++ OT_Website_Frontend/src/pages/Verification.jsx
@@ -4,14 +4,16 @@ import { useNavigate } from 'react-router-dom';
 function Verification() {
 
     const navigate = useNavigate();
+
+    const handleButtonClick = () => {
+        navigate('/settings');
+    };
     
     return (
-
         <div className="verification">
-            
-        <h3>You have made successfully made an account!</h3>
-        <p>Please wait for your account to be fully verified by an eboard member. You can upload your profile picture and LinkedIn profile link while you wait.</p>
-        <button onClick={navigate('/settings')}>I understand.</button>
+            <h3>You have made successfully made an account!</h3>
+            <p>Please wait for your account to be fully verified by an eboard member. You can upload your profile picture and LinkedIn profile link while you wait.</p>
+            <button onClick={handleButtonClick}>I understand.</button>
         </div>
     )
 }
