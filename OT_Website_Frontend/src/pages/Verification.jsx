2520e99e03de3e5e0f9f721ba3a0c6132e2e357b
yuanjiex21 <jayxu9000@gmail.com>
2024-01-06 01:29:44 -0500

    Added a temporary message to the verification page.

--- OT_Website_Frontend/src/pages/Verification.jsx
+++ OT_Website_Frontend/src/pages/Verification.jsx
@@ -12,7 +12,7 @@ function Verification() {
     return (
         <div className="verification">
             <h3>You have made successfully made an account!</h3>
-            <p>Please wait for your account to be fully verified by an eboard member. You can upload your profile picture and LinkedIn profile link while you wait.</p>
+            <p>Please wait a bit for your account to be verified or contact Yuanjie for any further inquiries.</p>
             <button onClick={handleButtonClick}>I understand.</button>
         </div>
     )
